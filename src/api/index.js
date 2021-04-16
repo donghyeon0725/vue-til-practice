cf3cc447ec84da000f4ed31a20b6057aa745df8d
donghyeon0725 <ehdgus5015@gmail.com>
2021-04-17 05:54:33 +0900

    git add 화면에 게시물을 보여주고, 스피너를 통해서 로딩 상태임을 알릴 수 있는 방법을 추가함

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
+import { setInterceptors } from './common/interceptors';
 
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-});
+// 조금 더 깔끔한 코드 작성을 위해서 함수로 묶었다.
+function getInstance() {
+  const instance = axios.create({
+    baseURL: process.env.VUE_APP_API_URL,
+  });
+  return setInterceptors(instance);
+}
+const instance = getInstance();
 
+// 회원가입 api
 function registerUser(userData) {
   return instance.post('signup', userData);
 }
 
+// 로그인 api
 function loginUser(userData) {
   return instance.post('login', userData);
 }
 
-export { registerUser, loginUser };
+// 학습 노트 데이터를 조회하는 api
+function fetchPosts() {
+  return instance.get('posts');
+}
+
+export { registerUser, loginUser, fetchPosts };
