896de2ba23dc1640626f1a7440da38e60fcdf884
donghyeon0725 <ehdgus5015@gmail.com>
2021-04-16 17:50:10 +0900

    git add vuex를 이용한 로그인 기능 추가

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -56,8 +56,10 @@ export default {
           password: this.password,
         };
         const { data } = await loginUser(userData);
-        console.log(data.user.username);
-        this.logMessage = `${data.user.username} 님 환영합니다`;
+        this.$store.commit('setUsername', data.user.username);
+        // 메인 페이지로 이동
+        this.$router.push('/main');
+        // this.logMessage = `${data.user.username} 님 환영합니다`;
         // this.initForm();
       } catch (error) {
         // 에러 핸들링할 코드
