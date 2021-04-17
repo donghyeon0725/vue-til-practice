5643214aee5f91ad9fa5b245b01ce54631dcab57
donghyeon0725 <ehdgus5015@gmail.com>
2021-04-17 18:58:11 +0900

    git add 포스트의 DELETE 기능 추가

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -8,16 +8,26 @@
     </div>
     <div class="post-time">
       {{ postItem.createdAt }}
+      <i class="icon ion-md-create"></i>
+      <i class="icon ion-md-trash" @click="deleteItem"></i>
     </div>
   </li>
 </template>
 
 <script>
+import { deletePost } from '@/api/posts';
 export default {
   props: {
     postItem: {
       type: Object,
       required: true,
+    },
+  },
+  methods: {
+    async deleteItem() {
+      // this.postItem._id 으로 부모 모듈로 부터 받은 값에 직접 접근이 가능하다.
+      await deletePost(this.postItem._id);
+      console.log('deleted');
     },
   },
 };
